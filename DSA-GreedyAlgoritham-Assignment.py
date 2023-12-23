f84d5935d2b61634b84af3ff933213ddd8f0d9d6
Imesha Dilshani <93858302+ImeshaDilshani@users.noreply.github.com>
2023-12-23 20:47:31 +0530

    Add files via upload

--- DSA-GreedyAlgoritham-Assignment.py
+++ DSA-GreedyAlgoritham-Assignment.py
@@ -1,23 +1,32 @@
-def efficient_interval_cover(nums):
+def efficient_interval_cover(*xn):
+    nums = list(xn)
+    n = len(nums)
+
     nums.sort()
     intervals = []
-    
+    m = 0
     current_interval_start = nums[0]
 
-    for num in nums[1:]:
+    for i in range(1, n):
+        num = nums[i]
         if num < current_interval_start + 1:
             continue
         else:
             intervals.append((current_interval_start, current_interval_start + 1))
+            m += 1
             current_interval_start = num
-    
-    intervals.append((current_interval_start, current_interval_start + 1)) 
-    return intervals
 
-userInput = input("Enter real numbers separated by spaces: ")
-inputNumbers = [float(x) for x in userInput.split()]
+    intervals.append((current_interval_start, current_interval_start + 1))
+    m += 1
 
-# input_numbers = [0.1, 0.9, 1.1, 1.555]
-intervals = efficient_interval_cover(inputNumbers)
-print("Minimum number of intervals:", len(intervals))
+    return intervals, m
+
+
+n = int(input("Enter the value of n (number of real numbers): "))
+xn = []
+for i in range(n):
+    xn.append(float(input(f"Enter x{i + 1}: ")))
+
+intervals, m = efficient_interval_cover(*xn)
+print("Minimum number of unit intervals(m):", m)
 print("Intervals:", intervals)
